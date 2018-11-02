37e07798aebd66ae71e650b69944403b1f4893cc
Nikos <nik0sc@users.noreply.github.com>
2018-11-02 12:38:27 +0800

    vertexLowLink, solution both remember insertion order (topological order)

--- src/TarjanSccFinder.java
+++ src/TarjanSccFinder.java
@@ -32,7 +32,9 @@ public class TarjanSccFinder {
     Low-link value for each vertex
     This eventually contains the information about which components are
     strongly connected.
-    Strongly connected component vertices all hvae the same low-link value
+    Strongly connected component vertices all have the same low-link value
+    Tarjan's algorithm outputs vertices in reverse topological order.
+    So this Map remembers the insertion order of vertices.
     */
     private Map<Integer, Integer> vertexLowLink;
 
@@ -48,7 +50,14 @@ public class TarjanSccFinder {
     // Next available id for assignment
     private int nextId;
 
-    private Collection<List<Integer>> solution;
+    /*
+    'reversed' version of vertexLowLink.
+    Where {k->v, k->v, ...} now we have {v->[k1,k2...], v->[...], ...}
+    The keys are the low-link values (not directly useful for assignment!)
+    The values of this map are the strongly connected components.
+    Like vertexLowLink, remembers insertion order.
+     */
+    private Map<Integer, List<Integer>> solution;
 
     /**
      * Create a new instance of Tarjan's algorithm-based solver.
@@ -59,23 +68,23 @@ public class TarjanSccFinder {
         stack = new ArrayDeque<>();
         vertexToId = new HashMap<>();
         vertexOnStack = new HashMap<>();
-        vertexLowLink = new HashMap<>();
+        vertexLowLink = new LinkedHashMap<>();
         nextId = 0;
 
         // Populate vertexToId with sentinels and vertexOnStack with false
         // vertexToId and vertexOnStack should contain even the (unseen) inverses
         for (int vertex: graph.keySet()) {
             vertexToId.put(vertex, -1);
-//            vertexLowLink.put(vertex, -1);
             vertexOnStack.put(vertex, false);
         }
     }
 
     /**
-     * Run Tarjan's algorithm.
-     * @return a Collection of List<Integer>s where each list is a strongly connected component
+     * Run Tarjan's algorithm. The returned Collection returns components in ascending order of their indexes
+     * (the topological sort property which is useful for assignment later)
+     * @return an ordered Collection of List<Integer>s where each list is a strongly connected component
      */
-    public Collection<List<Integer>> findSccs() {
+    public Map<Integer, List<Integer>> findSccs() {
         // Prefer the existing solution
         if (solution != null) {
             return solution;
@@ -93,8 +102,10 @@ public class TarjanSccFinder {
         // Now we need to "reverse" it and put the values into solution
         // Eg. {1->2, 2->3, 3->2} reverses to {2->[1,3], 3->[2]}
         // If there are N entries in vertexLowLink then I think this is O(N)
-        Map<Integer, List<Integer>> lowLinkReversed = new HashMap<>();
+        Map<Integer, List<Integer>> lowLinkReversed = new LinkedHashMap<>();
 
+        // vertexLowLink remembers the insertion order of its items. So the
+        // reversal should also remember insertion order!
         for (Map.Entry<Integer, Integer> entry: vertexLowLink.entrySet()) {
             if (!lowLinkReversed.containsKey(entry.getValue())) {
                 lowLinkReversed.put(entry.getValue(), new ArrayList<>());
@@ -105,9 +116,12 @@ public class TarjanSccFinder {
             lowLinkReversed.get(entry.getValue()).add(entry.getKey());
         }
 
-        // Don't really care about the low-link values
-        solution = lowLinkReversed.values();
+        solution = lowLinkReversed;
         return solution;
+    }
+
+    public Map<Integer, Integer> getVertexLowLink() {
+        return vertexLowLink;
     }
 
     private void dfs(int vertex) {
